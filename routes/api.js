8746a6228774ed2967b810160b02a9738e734a15
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-26 13:29:46 +0200

    dependenices, gitignore, db connection

--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,15 @@
 'use strict';
 
+const expect = require('chai').expect;
+const mongodb = require('mongodb');
+const mongoose = require('mongoose');
+
+
+
 module.exports = function (app) {
+
+  let uri = process.env.DB
+  mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
   
   app.route('/api/threads/:board');
     
