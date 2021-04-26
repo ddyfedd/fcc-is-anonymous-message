bf6ac6e947245b2c336c12d24f2d2a836c1c66a9
ddyfedd <dzsamboki.techwriter@gmail.com>
2021-04-26 14:05:16 +0200

    helmet settings, schemas and models

--- routes/api.js
+++ routes/api.js
@@ -10,6 +10,26 @@ module.exports = function (app) {
 
   let uri = process.env.DB
   mongoose.connect(uri, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false });
+
+  let replySchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    delete_password: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    reported: {type: Boolean, required: true}
+  });
+
+  let threadSchema = new mongoose.Schema({
+    text: {type: String, required: true},
+    delete_password: {type: String, required: true},
+    board: {type: String, required: true},
+    created_on: {type: Date, required: true},
+    bumped_on: {type: Date, required: true},
+    reported: {type: Boolean, required: true},
+    replies: [replySchema]
+  });
+
+  let Reply = mongoose.model('Reply', replySchema);
+  let Thread = mongoose.model('Thread', threadSchema);
   
   app.route('/api/threads/:board');
     
